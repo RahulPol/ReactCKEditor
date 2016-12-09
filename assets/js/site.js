0063014ce05f9c483fae77beb7d9f3d61914ddeb
Rahul Pol <polrahul10@gmail.com>
2016-12-09 21:24:23 +0530

    Take it up from here

--- assets/js/site.js
+++ assets/js/site.js
@@ -6,9 +6,28 @@ $(function () {
         }
     }
 
+    var validateOption = function (selector) {
+        if (CKEDITOR.instances[selector].getData().length == 0) {
+            console.log("Please fill the option ", selector);
+        }
+    }
+
+    // var validateOptions = function () {
+    //     $('#OptionBoard').find('.option-box').each(function () {
+    //         validateOption($(this).attr('data-optionid'));
+    //     })
+    // }
+
     $('#saveQuestion').on('click', function () {
         validateQuestion();
 
+        $('#OptionBoard').find('.option-box').each(function () {
+            validateOption($(this).attr('data-optionid'));
+        })
+
+        validateOptions();
         console.log(CKEDITOR.instances.question.getData());
     });
+
+
 });
--- assets/jsx/Option.jsx
+++ assets/jsx/Option.jsx
@@ -1,23 +1,41 @@
 window.Option = React.createClass({
 
-    shouldComponentUpdate: function () {
-        return false;
-    },//shouldComponentUpdate
+    getInitialState: function () {
+        return { selected: false }
+    },
+
+    // shouldComponentUpdate: function () {
+    //     return false;
+    // },//shouldComponentUpdate
 
     componentDidMount: function () {
         CKEDITOR.replace(this.props.uniqueId);
     },//componentDidMount
 
+    onTickClicked: function () {
+        this.setState({ selected: !this.state.selected });
+    },//onTickClicked
 
     renderOption: function () {
+        var optionButtonClass = "btn btn-sm btn-default";
+        if (this.state.selected == true) {
+            optionButtonClass = "btn btn-sm btn-success";
+        } else {
+            optionButtonClass = "btn btn-sm btn-default";
+        }
+
         if (this.props.questionType == 'MCQ') {
             return (
                 <div>
-                    <div className="option-box mcq-option">
+                    <div className="option-box mcq-option" data-optionId={this.props.uniqueId} data-optionName={this.props.optionText} data-isSelected={this.state.selected}>
                         <div className="option-header">
                             <h3 className="option-text">
-                                Option {this.props.optionText}
-                                <span className="option-select pull-right">This option is correct</span>
+                                ({this.props.optionText}):
+                                <span className="option-select pull-right">
+                                    <button type="button" className={optionButtonClass} onClick={this.onTickClicked}>
+                                        <i className="fa fa-check" style={{ paddingRight: 10 + 'px' }} aria-hidden="true"></i>This answer option is correct
+                                    </button>
+                                </span>
                             </h3>
                         </div>
                         <textarea ref={this.props.id} id={this.props.uniqueId} rows="5" cols="50" >
